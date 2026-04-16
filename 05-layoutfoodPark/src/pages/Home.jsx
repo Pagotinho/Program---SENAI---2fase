4c5d5e208283c763658a0bb5d3339d406f433148
MATHEUS PARES PAGOTTO <matheus_pagotto@estudante.sesisenai.org.br>
2026-04-15 22:13:37 -0300

    fazendo o trabalho

--- 05-layoutfoodPark/src/pages/Home.jsx
+++ 05-layoutfoodPark/src/pages/Home.jsx
@@ -1,22 +1,13 @@
 import './Paginas.css'
-
+//Inicio da Pagina
 function Home() {
   return (
     <div className={'cont-home'}>
-
-    <h1>Bem-vindo ao foodtruck! 🚚🔥</h1>
-    <p>Aqui você encontra o melhor da comida de rua em um só lugar:
-      carnes suculentas direto da brasa, salmãos deliciosos e lanches absurdamente bons!</p>
-
-   
-
-<div className="menu">
-        <span>🍔 Lanchonete</span>
-        <span>🍖 Churrasco</span>
-        <span>🐟 Salmão</span>
-      </div>
-
+    <h1>Bem-vindo ao foodtruck! 🚚</h1>
+    <h3 className={'churras'}>Churrascaria do Gáucho</h3>
+    <h3 className={'lanchonete'}>Lanchonete</h3>
     </div>
+
   )
 }
 
