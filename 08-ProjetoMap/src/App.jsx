9833a5e9504aa9e3fc7e07a1fc6ccedb2cb49779
Pagotinho <paresmatheus@gmail.com>
2026-05-19 21:46:05 -0300

    Fazer Exercicio

---
+++ 08-ProjetoMap/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+
+  const [Armas,setArmas] = useState(0)
+  const [quantidade,setQuantidade] = useState([])
+  const [imagem,setImagem] = useState([])
+
+
+  return (
+  <div className="cont-main">
+  
+  </div>
+
+  )
+}
+
+export default App
---
+++ 08-ProjetoMap/src/components/Cadastro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cadastro() {
+    return (
+        <div className="cont-main">
+        <h1> Quantos bolos levarei? </h1>
+        
+        </div>
+    )
+}
+
+export default Cadastro
